f5a94c346dc5c149f7b885921f1a003dbdf1f369
Arvin <arvincris@gmail.com>
2024-09-30 16:27:29 +0800

    Building the App Layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from './AppLayout.module.css'
 
 function AppLayout() {
     return (
-        <div>
-            <AppNav></AppNav>
-            app
+        <div className={styles.app}>
+            <Sidebar></Sidebar>
+            <Map></Map>
         </div>
     )
 }
