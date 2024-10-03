740d2515833dc54e384f4bbc7a32f7d803bf6492
Arvin <arvincris@gmail.com>
2024-10-03 15:03:36 +0800

    Dynamic Routes with URL parameters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -46,6 +47,7 @@ function App() {
           <Route path="app" element={<AppLayout/>}>
             <Route index element={<CityList cities={cities} isLoading={isLoading} />}></Route>
             <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />}></Route>
+            <Route path="cities/:id" element={<City/>}></Route>
             <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>}></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
