e723442408e1307ace206179308afac8170b9bad
Arvin <arvincris@gmail.com>
2024-10-03 10:23:09 +0800

    Implementing the CitiesList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react";
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from './components/CityList';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function() {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        console.log(data);
+        setCities(data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+      finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -15,9 +41,9 @@ function App() {
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
           <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>List of Cities </p>}></Route>
-            <Route path="cities" element={<p>List of Cities</p>}></Route>
+          <Route path="app" element={<AppLayout/>}>
+            <Route index element={<CityList cities={cities} isLoading={isLoading} />}></Route>
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />}></Route>
             <Route path="countries" element={<p>List of Countries</p>}></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar() {
             <Logo/>
             <AppNav/>
 
-            <p><Outlet/></p>
+            <Outlet/>
             <footer className={styles.footer}>
                 <p className={styles.copyright}>
                     &copy; Copyright {new Date().getFullYear()} by ArvinCarloCris Inc.
