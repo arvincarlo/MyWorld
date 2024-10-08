3c01d635ba4c88a474b8b51d8ec2a802e771c31a
Arvin <arvincris@gmail.com>
2024-10-08 11:37:20 +0800

    Setting the map position with Geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,39 @@
-import styles from './Map.module.css'
 import {useSearchParams, useNavigate} from 'react-router-dom';
 import { MapContainer, TileLayer, Popup, Marker, useMap, useMapEvents } from 'react-leaflet';
 import {useState, useEffect} from 'react';
 import {useCities} from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
 
+import styles from './Map.module.css';
+import Button from './Button';
 function Map() {
-    /*eslint-disable*/
-    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
+    const {cities, flagemojiToPNG} = useCities();
+    const [mapPosition, setMapPosition] = useState([14, 50]);
+    const {
+        isLoading: isLoadingPosition,
+        position: geolocationPosition,
+        getPosition,
+    } = useGeolocation();
+    
     const mapLat = searchParams.get('lat');
     const mapLng = searchParams.get('lng');
-    const {cities, flagemojiToPNG} = useCities();
-
-    const [mapPosition, setMapPosition] = useState([12, 121]);
 
     useEffect(function() {
-        if (mapLat, mapLng) setMapPosition([mapLat, mapLng])
-    }, [mapLat, mapLng])
+        if (mapLat, mapLng) {
+            setMapPosition([mapLat, mapLng]);
+        }
+    }, [mapLat, mapLng]);
+
+    useEffect(function() {
+        if (geolocationPosition) {
+            setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+        }
+    }, [geolocationPosition]);
 
     return (
         <div className={styles.mapContainer}>
+            {!geolocationPosition && <Button type='position' onClick={getPosition}>{isLoadingPosition ? 'Loading...' : 'Use your position'}</Button>}
             {/* <button onClick={() => setSearchParams(), setMapPosition()}></button> */}
             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
