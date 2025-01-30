dbeeda9695bb359ef667c0c042f194c0f728428f
Arvin Cris <arvincris97@gmail.com>
2025-01-30 18:34:30 +0800

    Content updates

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,7 +10,7 @@ export default function Homepage() {
         <h1>
           You travel the world.
           <br />
-          WanderWise keeps track of your adventures.
+          PinVoyage keeps track of your adventures.
         </h1>
         <h2>
           A world map that tracks your footsteps into every city you can think
